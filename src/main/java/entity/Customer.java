72816fb0a5cec094d4728901ca54b0f153c93821
Jesse <mailjesse99@gmail.com>
2020-07-29 11:31:27 +0200

    Override hashcode and equals

--- src/main/java/entity/Customer.java
+++ src/main/java/entity/Customer.java
@@ -3,6 +3,7 @@ package entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,86 +13,112 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import java.util.Objects;
 
 @Entity
 @Table(name = "customers")
 public class Customer {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name = "f_name")
-	private String firstName;
-	
-	@Column(name = "l_name")
-	private String lastName;
-	
-	@Column(unique = true)
-	private String email;
-	private String phone;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	LocalDateTime created;
-	
-	@OneToMany(mappedBy = "customer")
-	private List<Order> orders;
-	
-	public Customer(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	
-	public Customer() {}
 
-	public Long getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    @Column(name = "f_name")
+    private String firstName;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    @Column(name = "l_name")
+    private String lastName;
 
-	public String getLastName() {
-		return lastName;
-	}
+    @Column(unique = true)
+    private String email;
+    private String phone;
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    @Temporal(TemporalType.TIMESTAMP)
+    LocalDateTime created;
 
-	public String getEmail() {
-		return email;
-	}
+    @OneToMany(mappedBy = "customer", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Order> orders;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-	public String getPhone() {
-		return phone;
-	}
+    protected Customer() {
+    }
 
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public LocalDateTime getCreated() {
-		return created;
-	}
-	
-	public List<Order> getOrders() {
-		return orders;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
-	
-	
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.email.toLowerCase());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Customer other = (Customer) obj;
+        if (!Objects.equals(this.getEmail().toLowerCase(), other.getEmail().toLowerCase())) {
+            return false;
+        }
+        return true;
+    }
+
 }
